1b4b22495642bc02e18d6d5d78714943f0d2e839
Marianna Dei <d.marianna@icloud.com>
2024-06-20 02:13:48 +0300

    Updated the project template + added code to contactsOps.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,52 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
-import persistReducer from 'redux-persist/es/persistReducer';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfigContacts = {
-  key: 'contacts',
-  storage,
-  whitelist: ['contacts'],
-};
-
-const persistConfigFilters = {
-  key: 'filters',
-  storage,
-};
-
-const persistedReducerContacts = persistReducer(
-  persistConfigContacts,
-  contactsReducer
-);
-
-const persistedReducerFilters = persistReducer(
-  persistConfigFilters,
-  filtersReducer
-);
-
-const rootReducer = {
-  contacts: persistedReducerContacts,
-  filters: persistedReducerFilters,
-};
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
 });
-
-export const persistor = persistStore(store);
